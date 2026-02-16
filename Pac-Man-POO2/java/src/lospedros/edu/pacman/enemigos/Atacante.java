66b8a6d964fe7d728238670adfde6c1cc18bc2e6
notdantereach <gamersthemexicans@gmail.com>
2026-02-15 20:12:42 -0600

    Updated Atacante.java with new implementation.

--- Pac-Man-POO2/java/src/lospedros/edu/pacman/enemigos/Atacante.java
+++ Pac-Man-POO2/java/src/lospedros/edu/pacman/enemigos/Atacante.java
@@ -43,7 +43,7 @@ public abstract class Atacante {
         return x;
     }
 
-    protected void setX(int x) {
+    public void setX(int x) {
         this.x = x;
     }
 
@@ -51,7 +51,7 @@ public abstract class Atacante {
         return y;
     }
 
-    protected void setY(int y) {
+    public void setY(int y) {
         this.y = y;
     }
 
@@ -59,7 +59,7 @@ public abstract class Atacante {
         return velocidad;
     }
 
-    protected void setVelocidad(int velocidad) {
+    public void setVelocidad(int velocidad) {
         this.velocidad = velocidad;
     }
 
