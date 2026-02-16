02443ff51a56d8202ac9829edd6acd87f4063a58
notdantereach <gamersthemexicans@gmail.com>
2026-02-15 18:47:45 -0600

    Updated Entity.java to add private fields and getters/setters for x, y, speed, direction, solidArea, and collisionOn.

--- Pac-Man-POO2/java/src/lospedros/edu/pacman/data/Entity.java
+++ Pac-Man-POO2/java/src/lospedros/edu/pacman/data/Entity.java
@@ -1,12 +1,58 @@
-package lospedros.edu.pacman.data;
-
-import java.awt.Rectangle;
-
 public class Entity {
-    public int x, y;
-    public int speed;
-    
-    public String direction;
-    public Rectangle solidArea;
-    public boolean collisionOn = false;
+    // Private fields
+    private int x;
+    private int y;
+    private int speed;
+    private String direction;
+    private Rectangle solidArea;
+    private boolean collisionOn;
+
+    // Getters and Setters
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public int getSpeed() {
+        return speed;
+    }
+
+    public void setSpeed(int speed) {
+        this.speed = speed;
+    }
+
+    public String getDirection() {
+        return direction;
+    }
+
+    public void setDirection(String direction) {
+        this.direction = direction;
+    }
+
+    public Rectangle getSolidArea() {
+        return solidArea;
+    }
+
+    public void setSolidArea(Rectangle solidArea) {
+        this.solidArea = solidArea;
+    }
+
+    public boolean isCollisionOn() {
+        return collisionOn;
+    }
+
+    public void setCollisionOn(boolean collisionOn) {
+        this.collisionOn = collisionOn;
+    }
 }
