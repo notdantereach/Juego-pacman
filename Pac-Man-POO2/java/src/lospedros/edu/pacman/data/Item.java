b2728ea518cced02efc75d57a6f57fb5488cba0f
notdantereach <gamersthemexicans@gmail.com>
2026-02-15 19:14:29 -0600

    Update Item.java to implement Item class with proper attributes and methods

--- Pac-Man-POO2/java/src/lospedros/edu/pacman/data/Item.java
+++ Pac-Man-POO2/java/src/lospedros/edu/pacman/data/Item.java
@@ -1,15 +1,41 @@
 package lospedros.edu.pacman.data;
 
 public class Item {
-    public int x, y;
-    public int points;
-    public boolean isBonus; //
-    public boolean collected = false;
+    private final int x;
+    private final int y;
+    private final int points;
+    private final boolean bonus;
+    private boolean collected;
 
-    public Item(int x, int y, int points, boolean isBonus) {
+    public Item(int x, int y, int points, boolean bonus) {
         this.x = x;
         this.y = y;
         this.points = points;
-        this.isBonus = isBonus;
+        this.bonus = bonus;
+        this.collected = false;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public int getPoints() {
+        return points;
+    }
+
+    public boolean isBonus() {
+        return bonus;
+    }
+
+    public boolean isCollected() {
+        return collected;
+    }
+
+    public void setCollected(boolean collected) {
+        this.collected = collected;
     }
 }
