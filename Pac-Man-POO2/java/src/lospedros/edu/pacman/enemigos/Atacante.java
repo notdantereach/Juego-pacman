31a12d91c6e84908197fa3e08c879f31db461f6e
notdantereach <gamersthemexicans@gmail.com>
2026-02-15 19:19:04 -0600

    Updated Atacante.java to use colored circles instead of images

--- Pac-Man-POO2/java/src/lospedros/edu/pacman/enemigos/Atacante.java
+++ Pac-Man-POO2/java/src/lospedros/edu/pacman/enemigos/Atacante.java
@@ -1,14 +1,17 @@
 package lospedros.edu.pacman.enemigos;
 
-import java.awt.*;
+import java.awt.Color;
+import java.awt.Graphics;
 
 /**
  * Clase abstracta Atacante simplificada.
  * Ahora utiliza círculos de colores en lugar de imágenes.
  */
 public abstract class Atacante {
-    protected int x, y, velocidad;
-    protected Color color; // Atributo para definir el color del punto
+    private int x;
+    private int y;
+    private int velocidad;
+    private final Color color;
 
     // El constructor ya no recibe un String de imagen, sino un objeto Color
     public Atacante(int x, int y, int velocidad, Color color) {
@@ -30,16 +33,37 @@ public abstract class Atacante {
     }
 
     protected double calcularDistancia(int objetivoX, int objetivoY) {
-        return Math.sqrt(Math.pow(objetivoX - this.x, 2) + Math.pow(objetivoY - this.y, 2));
+        return Math.sqrt(Math.pow(objetivoX - x, 2) + Math.pow(objetivoY - y, 2));
     }
 
     public abstract void calcularSiguienteMovimiento(int pacmanX, int pacmanY);
 
     // Getters y Setters
-    public int getX() { return x; }
-    public void setX(int x) { this.x = x; }
-    public int getY() { return y; }
-    public void setY(int y) { this.y = y; }
-    public int getVelocidad() { return velocidad; }
-    public void setVelocidad(int velocidad) { this.velocidad = velocidad; }
+    public int getX() {
+        return x;
+    }
+
+    protected void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    protected void setY(int y) {
+        this.y = y;
+    }
+
+    public int getVelocidad() {
+        return velocidad;
+    }
+
+    protected void setVelocidad(int velocidad) {
+        this.velocidad = velocidad;
+    }
+
+    public Color getColor() {
+        return color;
+    }
 }
