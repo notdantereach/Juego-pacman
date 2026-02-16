677386a340b426e7135f27607dbf9d78548d8244
notdantereach <gamersthemexicans@gmail.com>
2026-02-15 19:21:55 -0600

    Update Tile.java file with the latest implementation

--- Pac-Man-POO2/java/src/lospedros/edu/pacman/tile/Tile.java
+++ Pac-Man-POO2/java/src/lospedros/edu/pacman/tile/Tile.java
@@ -3,6 +3,22 @@ package lospedros.edu.pacman.tile;
 import java.awt.image.BufferedImage;
 
 public class Tile {
-    public BufferedImage image;
-    public boolean collision = false;
+    private BufferedImage image;
+    private boolean collision;
+
+    public BufferedImage getImage() {
+        return image;
+    }
+
+    public void setImage(BufferedImage image) {
+        this.image = image;
+    }
+
+    public boolean isCollision() {
+        return collision;
+    }
+
+    public void setCollision(boolean collision) {
+        this.collision = collision;
+    }
 }
